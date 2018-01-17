d6f3b7aea3c60ca9df94e3e89a5e6a662b0832ba
Omar <ocontreras.sf@gmail.com>
2018-01-17 20:57:43 +0000

    Add double quote

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/**
+/*
  * main - Entry point
  *
  * prints "Programming is like building a multilingual puzzle"
@@ -9,6 +9,6 @@
  */
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle");
+  puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }
