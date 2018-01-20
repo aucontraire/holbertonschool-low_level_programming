bdcfc62fb7ed9b35d5a50ef54027a80620aefb1b
Omar <ocontreras.sf@gmail.com>
2018-01-20 00:42:12 +0000

    Add Intel

--- 0x00-hello_world/main.s
+++ 0x00-hello_world/main.s
@@ -1,17 +1,18 @@
 	.file	"main.c"
+	.intel_syntax noprefix
 	.text
 	.globl	main
 	.type	main, @function
 main:
 .LFB0:
 	.cfi_startproc
-	pushq	%rbp
+	push	rbp
 	.cfi_def_cfa_offset 16
 	.cfi_offset 6, -16
-	movq	%rsp, %rbp
+	mov	rbp, rsp
 	.cfi_def_cfa_register 6
-	movl	$0, %eax
-	popq	%rbp
+	mov	eax, 0
+	pop	rbp
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
