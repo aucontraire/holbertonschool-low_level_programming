77f03866b286fdd6f1a708cc128516e2264b9083
Omar <ocontreras.sf@gmail.com>
2018-01-23 17:45:39 +0000

    Use separate putchar for new line

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -10,13 +10,14 @@
 
 int main(void)
 {
-	char text[11] = "Holberton\n";
+	char text[10] = "Holberton";
 	int i = 0;
 
-	for (i = 0; i < 11; i++)
+	for (i = 0; i < 10; i++)
 	{
 		_putchar(text[i]);
 	}
+	_putchar('\n');
 
 	return (0);
 }
