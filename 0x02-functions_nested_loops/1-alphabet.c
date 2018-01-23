d5d433c461927e8db702b72691962f1610b18b28
Omar <ocontreras.sf@gmail.com>
2018-01-23 02:26:18 +0000

    Add function that prints the alphabet, in lowercase, followed by a new line

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - print alphabet
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 # Functions & Nested Loops
 * 0-holberton.c: program that prints Holberton, followed by a new line
+* 1-alphabet.c: function that prints the alphabet, in lowercase, followed by a new line
