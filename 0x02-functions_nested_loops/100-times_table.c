3b58980a4aa2eb209daff59433a1ced8a55f1a59
Omar <ocontreras.sf@gmail.com>
2018-01-24 21:53:50 +0000

    Correct function name in comments

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * times_table - prints the times table
+ * print_times_table - prints the times table
  * @n: integer for which the times table will be printed
  *
  * Description: prints the times table
