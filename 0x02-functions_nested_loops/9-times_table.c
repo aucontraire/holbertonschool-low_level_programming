029e1c27e3c458b45eda65cbe6356b9778a180a0
Omar <ocontreras.sf@gmail.com>
2018-01-24 06:07:50 +0000

    Add function that prints the 9 times table, starting with 0

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * times_table - prints the 9 times table
+ *
+ * Description: prints the 9 times table
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int row, column, product, tens, ones = 0;
+
+	while (row <= 9)
+	{
+		while (column <= 9)
+		{
+			product = row * column;
+			tens = product / 10;
+			ones = product % 10;
+
+			if (column == 0) {
+				_putchar('0');
+			}
+			else if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(ones +'0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(tens + '0');
+				_putchar(ones + '0');
+			}
+			column++;
+		}
+		column = 0;
+		_putchar('\n');
+		row++;
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -8,3 +8,4 @@
 * 6-abs.c: function that computes the absolute value of an integer
 * 7-print_last_digit.c: function that prints the last digit of a number
 * 8-24_hours.c: function that prints every minute of the day
+* 9-times_table.c: function that prints the 9 times table, starting with 0
