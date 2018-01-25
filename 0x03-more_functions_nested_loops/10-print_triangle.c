be8c431abb81b3519940b6986bf70605f5008dec
Omar <ocontreras.sf@gmail.com>
2018-01-25 22:56:50 +0000

    Add function that prints a triangle

---
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: length of sides minus hypotenuse
+ *
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int line = 0;
+	int spaces;
+	int hashes;
+
+	if (size > 0)
+	{
+		while (line < size)
+		{
+			for (spaces = size - 1; spaces > line; spaces--)
+			{
+				_putchar(' ');
+			}
+			for (hashes = 0; hashes < line + 1; hashes++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+			line++;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
