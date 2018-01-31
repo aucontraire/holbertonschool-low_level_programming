aedc20ac735a6b92a9bf4205439786159790554e
Omar <ocontreras.sf@gmail.com>
2018-01-31 19:17:11 +0000

    Fix formatting/style issues

--- 0x04-pointers_arrays_strings/5-rev_string.c
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -10,14 +10,14 @@ void rev_string(char *s)
 {
 	int i, max, half;
 	char first, last;
-	i = 0;
 
+	i = 0;
 	while (s[i] != '\0')
 	{
 		i++;
 	}
 	max = i - 1;
-	half = max /2;
+	half = max / 2;
 	while (half >= 0)
 	{
 		first = s[max - half];
