589001b3f5d493cebcfea278c497e84c1cb6b259
Omar <ocontreras.sf@gmail.com>
2018-01-31 19:14:34 +0000

    Make more flexible to different size of strings

--- 0x04-pointers_arrays_strings/5-rev_string.c
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -8,11 +8,16 @@
 
 void rev_string(char *s)
 {
-	int max = 10 - 2;
-	int half = max / 2;
-	char first;
-	char last;
+	int i, max, half;
+	char first, last;
+	i = 0;
 
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	max = i - 1;
+	half = max /2;
 	while (half >= 0)
 	{
 		first = s[max - half];
