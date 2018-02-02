840599bad78ef8c525bf1fe747d7fe73c4f46423
Omar <ocontreras.sf@gmail.com>
2018-02-02 22:25:03 +0000

    Add function that reverses the content of an array of integers

---
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - reverses array
+ * @a: array of integers
+ * @n: number of elements in array
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int half = n / 2;
+	int left, right, i;
+	n = n - 1;
+
+	for (i = 0; i < half + 1; i++)
+	{
+		left = a[n - i];
+		right = a[i];
+		a[i] = left;
+		a[n - i] = right;
+	}
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -2,3 +2,5 @@
 * 0-strcat.c: function that concatenates two strings
 * 1-strncat.c: function that concatenates two strings
 * 2-strncpy.c: function that copies a string
+* 3-strcmp.c: function that compares two strings
+* 4-rev_array.c: function that reverses the content of an array of integers
