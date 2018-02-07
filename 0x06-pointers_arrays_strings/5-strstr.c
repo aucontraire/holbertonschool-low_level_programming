dd2f52412ec8ffbd7fc76f9955783e8465c67448
Omar <ocontreras.sf@gmail.com>
2018-02-07 03:36:47 +0000

    Add  function that locates a substring

---
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @haystack: string to search through
+ * @needle: string to match
+ * Return: pointer to initial segment of haystack
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, start;
+	int nlen = 0;
+	i = 0;
+	j = 0;
+	start = 0;
+
+	while (needle[nlen] != '\0')
+	{
+		nlen++;
+	}
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; j < nlen && haystack[i] == needle[j]; j++, i++)
+		{
+			if (j == 0)
+			{
+				start = i;
+			}
+			if (j == nlen - 1)
+			{
+				return (haystack + start);
+			}
+		}
+	}
+	return (0);
+}
