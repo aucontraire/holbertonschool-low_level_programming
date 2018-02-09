48d951baecd67d9d213a195506047ef5a1004969
Omar <ocontreras.sf@gmail.com>
2018-02-09 18:53:24 +0000

    Add program that prints its name

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - prints program name
+ * Return: (0)
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
--- 0x09-argc_argv/README.md
+++ 0x09-argc_argv/README.md
@@ -1,1 +1,2 @@
 # argc, argv
+* 0-whatsmyname.c: program that prints its name
