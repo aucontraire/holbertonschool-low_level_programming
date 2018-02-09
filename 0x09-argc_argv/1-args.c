ee5ef14d969148a16c4d34c6ece60d56b63c6b4e
Omar <ocontreras.sf@gmail.com>
2018-02-09 19:04:33 +0000

    Add program that prints the number of arguments passed into it

---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it
+ * Return: (0)
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
--- 0x09-argc_argv/README.md
+++ 0x09-argc_argv/README.md
@@ -1,2 +1,3 @@
 # argc, argv
 * 0-whatsmyname.c: program that prints its name
+* 1-args.c: program that prints the number of arguments passed into it
