723b93b00a6d60fe3b2b0d573ac7eafd4d65a60e
Omar <ocontreras.sf@gmail.com>
2018-02-10 00:40:16 +0000

    Add program that prints the minimum number of coins to make change for an amount of money

---
+++ 0x09-argc_argv/100-change.c
@@ -0,0 +1,80 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * make_change - finds least number of coins
+ * @n: money amount
+ * Return: number of coins
+ */
+
+int make_change(int n)
+{
+	int coins, quarters, dimes, nickels, twocents, pennies;
+
+	coins = 0;
+	quarters = 0;
+	dimes = 0;
+	nickels = 0;
+	pennies = 0;
+
+	while (n > 0)
+	{
+		if (n >= 25)
+		{
+			n -= 25;
+			quarters += 1;
+		}
+		else if (n >= 10)
+		{
+			n -= 10;
+			dimes += 1;
+		}
+		else if (n >= 5)
+		{
+			n -= 5;
+			nickels += 1;
+		}
+		else if (n >= 2)
+		{
+			n -= 2;
+			twocents += 1;
+		}
+		else
+		{
+			n -= 1;
+			pennies += 1;
+		}
+	}
+	coins = quarters + dimes + nickels + twocents + pennies;
+
+	return (coins);
+}
+
+/**
+ * main - prints minimum number of coins to make change
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	int coins;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else if (atoi(argv[1]) < 0)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		coins = make_change(atoi(argv[1]));
+		printf("%d\n", coins);
+	}
+	return (0);
+}
--- 0x09-argc_argv/README.md
+++ 0x09-argc_argv/README.md
@@ -4,3 +4,4 @@
 * 2-args.c: program that prints all arguments it receives
 * 3-mul.c: program that multiplies two numbers
 * 4-add.c: program that adds positive numbers
+* 100-change.c: program that prints the minimum number of coins to make change for an amount of money
