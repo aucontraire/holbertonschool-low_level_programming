1ffa2cce8808be81161413cf5d7a72e779d48dc2
Omar <ocontreras.sf@gmail.com>
2018-02-13 00:11:16 +0000

    Add function that copies the string pointed to by src to dest

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -1,25 +1,21 @@
 #include "holberton.h"
 
 /**
- * _strcpy - copies string pointed to by src to dest
- * @dest: destination char
- * @src: source char
- * Return: char copy
+ * _strcpy - copies the string pointed to by src into dest
+ * @dest: destination
+ * @src: source
+ * Return: char with copy of string
  */
 
 char *_strcpy(char *dest, char *src)
 {
-	int n = 0;
 	int i;
 
-	while (src[n] != '\0')
+	for (i = 0; *(src + i) != '\0'; i++)
 	{
-		n++;
+		dest[i] = *(src + i);
 	}
+	dest[i] = '\0';
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-	for ( ; i < n; i++)
-		dest[i] = '\0';
 	return (dest);
 }
