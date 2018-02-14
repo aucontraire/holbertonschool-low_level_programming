b2b8722c48bdaffe8990883b1e42dd36d79b4d9b
Omar <ocontreras.sf@gmail.com>
2018-02-14 05:19:44 +0000

    Add function that concatenates two strings

---
+++ 0x0A-malloc_free/2-str_concat.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two string
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer to new string, NULL if it fails
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int length1, length2, length3, i;
+	char *arr;
+
+	length1 = 0;
+	while (*(s1 + length1) != '\0')
+	{
+		length1++;
+	}
+
+	length2 = 0;
+	while (*(s2 + length2) != '\0')
+	{
+		length2++;
+	}
+	length3 = length1 + length2;
+	arr = (char*) malloc(length3 * sizeof(char));
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < length1; i++)
+		arr[i] = s1[i];
+	for (i = 0; i < length2; i++)
+		arr[i + length1] = s2[i];
+	return (arr);
+}
--- 0x0A-malloc_free/README.md
+++ 0x0A-malloc_free/README.md
@@ -1,3 +1,4 @@
 # Malloc, Free
 * 0-create_array.c: function that creates an array of chars, and initializes it with a specific char
 * 1-strdup.c: function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter
+* 2-str_concat.c: function that concatenates two strings
