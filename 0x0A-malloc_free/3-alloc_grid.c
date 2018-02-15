a13b840da2b1b27ab304c1be3ff7f98df88665af
Omar <ocontreras.sf@gmail.com>
2018-02-15 00:16:56 +0000

    Add edge case conditions

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -1,16 +1,34 @@
 #include "holberton.h"
 #include <stdlib.h>
-#include <stdio.h>
+
+/**
+ * alloc_grid - returns a pointer to a 2D array of integers
+ * @width: number of columns
+ * @height: number of rows
+ * Return: pointer to 2D array
+ */
 
 int **alloc_grid(int width, int height)
 {
 	int **grid, i;
 
-	grid =  (int**) malloc(height * sizeof(int*));
+	grid =  (int **) malloc(height * sizeof(int *));
+	if (grid == NULL)
+		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			while (i >= 0)
+			{
+				free(grid[i]);
+				i--;
+			}
+			free(grid);
+			return (NULL);
+		}
 	}
 	return (grid);
 }
