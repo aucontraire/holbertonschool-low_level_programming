4f3a8748771cca4f9fd7b3921e7c4df3e3fed926
Omar <ocontreras.sf@gmail.com>
2018-02-15 22:20:13 +0000

    Add function that allocates memory for an array

---
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: number of members
+ * @size: number of bytes
+ * Return: pointer to allocated memory corresponding with the string
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *arr;
+	unsigned int i;
+	char *k;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	arr = malloc(size * nmemb);
+	if (arr == NULL)
+		return (NULL);
+
+	k = arr;
+	for (i = 0; i < nmemb; i++)
+		k[i] = 0;
+	return (arr);
+}
--- 0x0B-more_malloc_free/README.md
+++ 0x0B-more_malloc_free/README.md
@@ -1,3 +1,4 @@
 # More Malloc, Free
 * 0-malloc_checked.c: function that allocates memory using malloc
 * 1-string_nconcat.c: function that concatenates two strings
+* 2-calloc.c: function that allocates memory for an array, using malloc
