3044da29aacf8ffe21ae9fa719cce8d394fdf259
Omar <ocontreras.sf@gmail.com>
2018-02-15 22:24:48 +0000

    Add the rest of places

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -22,7 +22,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 
 	k = arr;
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < nmemb * size; i++)
 		k[i] = 0;
 	return (arr);
 }
