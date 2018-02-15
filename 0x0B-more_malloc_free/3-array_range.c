f80999581eba0c1a774f344908f612eef86ead4e
Omar <ocontreras.sf@gmail.com>
2018-02-15 22:53:24 +0000

    Account for NULL

--- 0x0B-more_malloc_free/3-array_range.c
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -16,6 +16,8 @@ int *array_range(int min, int max)
 		return (NULL);
 	diff = max - min + 1;
 	arr = malloc(diff * sizeof(int));
+	if (arr == NULL)
+		return (NULL);
 
 	for (num = min, j = 0; num <= max && j < diff; num++, j++)
 		arr[j] = num;
