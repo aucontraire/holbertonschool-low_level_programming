c36c8739ca117d1dc2a4bc21f1b430572432f96c
Omar <ocontreras.sf@gmail.com>
2018-02-20 01:22:47 +0000

    Add new type dog_t

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -7,12 +7,12 @@ int _putchar(char c);
  * @age: doggo's age
  * @owner: owner's name
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif /* !DOG_H */
