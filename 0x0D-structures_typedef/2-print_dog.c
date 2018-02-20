7ca98a91c69f7267f3b850d7670afb04ee7c1696
Omar <ocontreras.sf@gmail.com>
2018-02-20 00:54:10 +0000

    Remove conditional

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -11,9 +11,8 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		if (d->name == NULL)
-			printf("Name: (nil)\n");
-		else
-			printf("Name: %s\n", d->name);
+			d->name = "(nil)";
+		printf("Name: %s\n", d->name);
 		printf("Age: %f\n", d->age);
 		if (d->owner == NULL)
 			printf("(nil)\n");
