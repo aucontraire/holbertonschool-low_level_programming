a1d96567bcb319a196d330939fc1b11a0e71299b
Omar <ocontreras.sf@gmail.com>
2018-02-20 01:18:35 +0000

    Going against the'funny' instructions

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -16,7 +16,7 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		printf("Age: %f\n", d->age);
 		if (d->owner == NULL)
-			printf("(nil)\n");
+			printf("Owner: (nil)\n");
 		else
 			printf("Owner: %s\n", d->owner);
 	}
