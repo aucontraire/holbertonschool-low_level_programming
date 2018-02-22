7561af09af753ff6052345e5d8a5a152d6a7ff11
Omar <ocontreras.sf@gmail.com>
2018-02-22 22:41:45 +0000

    Add checks for NULL

--- 0x0E-function_pointers/0-print_name.c
+++ 0x0E-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  * print_name - prints a name
@@ -9,5 +10,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (f == NULL)
+		return;
 	f(name);
 }
