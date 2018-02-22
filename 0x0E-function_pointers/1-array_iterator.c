8f99c7627140887eda657a4cca92272ad88b9184
Omar <ocontreras.sf@gmail.com>
2018-02-22 22:45:46 +0000

    Add checks for NULL

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -13,6 +13,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
+	if (array == NULL || action == NULL)
+		return;
+
 	for (i = 0; i < size; i++)
 		action(array[i]);
 }
