f4ca7fa706eaa4f86a614525222eb1d5685c6c02
Omar <ocontreras.sf@gmail.com>
2018-02-23 21:53:04 +0000

    Add function that prints strings

---
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - prints strings
+ * @separator: char separator
+ * @n: number of parameters
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	char *string;
+
+	if (n == 0)
+		return;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		string = va_arg(args, char *);
+		if (string == NULL)
+			string = "(nil)";
+
+		if (i != n - 1)
+		{
+			if (separator == NULL)
+				printf("%s", string);
+			else
+				printf("%s%s", string, separator);
+		}
+		else
+			printf("%s\n", string);
+	}
+	va_end(args);
+}
--- 0x0F-variadic_functions/README.md
+++ 0x0F-variadic_functions/README.md
@@ -1,3 +1,4 @@
 # Variadic Functions
 * 0-sum_them_all.c: function that returns the sum of all its parameters
 * 1-print_numbers.c: function that prints numbers, followed by a new line
+* 2-print_strings.c: function that prints strings
