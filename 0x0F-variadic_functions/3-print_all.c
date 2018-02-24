4a2876c2a10c2f7422649aeeb92331f17494f463
Omar <ocontreras.sf@gmail.com>
2018-02-24 21:04:20 +0000

    Add function that prints anything*

---
+++ 0x0F-variadic_functions/3-print_all.c
@@ -0,0 +1,94 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * print_char - print char
+ * @sep: separator
+ * @args: list of arguments
+ */
+
+void print_char(char *sep, va_list args)
+{
+	printf("%s", sep);
+	printf("%c", va_arg(args, int));
+}
+
+/**
+ * print_int - print int
+ * @sep: separator
+ * @args: list of arguments
+ */
+
+void print_int(char *sep, va_list args)
+{
+	printf("%s", sep);
+	printf("%d", va_arg(args, int));
+}
+
+/**
+ * print_float - print float
+ * @sep: separator
+ * @args: list of arguments
+ */
+
+void print_float(char *sep, va_list args)
+{
+	printf("%s", sep);
+	printf("%f", va_arg(args, double));
+}
+
+/**
+ * print_string - print string
+ * @sep: separator
+ * @args: list of arguments
+ */
+
+void print_string(char *sep, va_list args)
+{
+	printf("%s", sep);
+	printf("%s", va_arg(args, char *));
+}
+
+/**
+ * print_all - prints anything*
+ * @format: list of types of arguments
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i, j;
+	char *sep;
+
+	op_t ops[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
+
+	va_start(args, format);
+	i = 0;
+	while (format[i] != '\0')
+	{
+		j = 0;
+		while (ops[j].c != NULL)
+		{
+			if (*ops[j].c == format[i])
+			{
+				if (i == 0)
+					sep = "";
+				else
+					sep = ", ";
+				ops[j].f(sep, args);
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
