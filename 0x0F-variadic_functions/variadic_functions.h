9dce761a97bfc47bb97bc4c2966519301688cba7
Omar <ocontreras.sf@gmail.com>
2018-02-24 21:09:02 +0000

    Add struct to header

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,8 +1,23 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+
+/**
+ * struct op - Struct op
+ *
+ * @c: The data type (char)
+ * @f: The function associated with it
+ */
+
+typedef struct op
+{
+	char *c;
+	void (*f)(char *sep, va_list args);
+} op_t;
+
 void print_all(const char * const format, ...);
 #endif
