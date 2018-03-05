3e18bed96f2deeffd414b2fe7bcdddf674d37948
Omar <ocontreras.sf@gmail.com>
2018-03-05 23:49:30 +0000

    Add function that inserts a new node at a given position

---
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position
+ * @head: head of linked list
+ * @idx: index to be inserted at
+ * @n: integer data for inserted node
+ * Return: address of new node or NULL if it fails
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *node, *new, *prev;
+	unsigned int i;
+
+	if (*head == NULL)
+		return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	node = *head;
+	prev = *head;
+	i = 0;
+	while (node != NULL)
+	{
+		if (i == idx)
+		{
+			new->next = node;
+			prev->next = new;
+			return (new);
+		}
+		prev = node;
+		node = node->next;
+		i++;
+	}
+	return (NULL);
+}
--- 0x12-more_singly_linked_lists/README.md
+++ 0x12-more_singly_linked_lists/README.md
@@ -8,3 +8,4 @@
 * 6-pop_listint.c: function that deletes the head node of a listint_t linked list, and returns the head node’s data (n)
 * 7-get_nodeint.c: function that returns the nth node of a listint_t linked list
 * 8-sum_listint.c: function that returns the sum of all the data (n) of a listint_t linked list
+* 9-insert_nodeint.c: function that inserts a new node at a given position
