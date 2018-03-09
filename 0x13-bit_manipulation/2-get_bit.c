bd1ccb65f79fb2a3b371795f03aff42635711316
Omar <ocontreras.sf@gmail.com>
2018-03-09 05:02:49 +0000

    Add function that returns the value of a bit at a given index

---
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <limits.h>
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: integer
+ * @index: index
+ * Return: integer
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int on;
+	unsigned int i;
+	unsigned long int x;
+
+	on = 0;
+	for (i = 63; i != UINT_MAX; i--)
+	{
+		x = (n >> i) & 1;
+		if (x == 1)
+		{
+			on = 1;
+			if (index > i)
+				return (-1);
+		}
+		if (on == 1 && i == index)
+			return (((n >> i) & 1));
+	}
+	return (0);
+}
--- 0x13-bit_manipulation/README.md
+++ 0x13-bit_manipulation/README.md
@@ -1,3 +1,4 @@
 # Bit manipulation
 * 0-binary_to_uint.c: function that converts a binary number to an unsigned int
 * 1-print_binary.c: function that prints the binary representation of a number
+* 2-get_bit.c: function that returns the value of a bit at a given index
