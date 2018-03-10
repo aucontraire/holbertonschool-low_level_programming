b4bb1c0cf3840089f1312bce16b295f5f9aa2899
Omar <ocontreras.sf@gmail.com>
2018-03-10 00:39:22 +0000

    Change to use bit manip

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <limits.h>
 
 /**
  * get_bit - returns the value of a bit at a given index
@@ -10,24 +9,7 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int on;
-	unsigned int i;
-	unsigned long int x;
-
-	if (index >= (sizeof(n) * 8))
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	on = 0;
-	for (i = 63; i != UINT_MAX; i--)
-	{
-		x = (n >> i) & 1;
-		if (x == 1)
-		{
-			on = 1;
-			if (index > i)
-				return (-1);
-		}
-		if (on == 1 && i == index)
-			return (((n >> i) & 1));
-	}
-	return (-1);
+	return ((n >> index) & 1);
 }
