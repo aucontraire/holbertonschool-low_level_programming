20ea7a21cae9d0a666e2720d30afaf4b3b7d2566
Omar <ocontreras.sf@gmail.com>
2018-03-13 21:57:41 +0000

    Add truncate flag

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -34,12 +34,15 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
 	if (text_content == NULL)
+	{
 		length = 0;
+		text_content = "";
+	}
 	else
 		length = _strlen(text_content);
 
