3357a09e3b59827908d28f3406b49fe3346c2a8c
Omar <ocontreras.sf@gmail.com>
2018-03-13 20:47:32 +0000

    Use STDOUT_FILENO instead

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -31,7 +31,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buffer[letters + 1] = '\0';
 	close(fd);
 
-	bytes_written = write(STDIN_FILENO, buffer, bytes_read);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 	if (bytes_written == -1)
 		return (0);
 
