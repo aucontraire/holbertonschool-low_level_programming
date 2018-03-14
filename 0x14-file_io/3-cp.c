8ea490e76bcbb1266103fee20dc6c9948089e613
Omar <ocontreras.sf@gmail.com>
2018-03-14 21:06:45 +0000

    Add program that copies the content of a file to another file

---
+++ 0x14-file_io/3-cp.c
@@ -0,0 +1,67 @@
+#include "holberton.h"
+#include <stdarg.h>
+
+/**
+ * error_handler - handles errors for cp
+ * @exit_code: exit code
+ * @message: error message
+ * @type: data type for format
+ */
+
+void error_handler(int exit_code, char *message, char type, ...)
+{
+	va_list args;
+
+	va_start(args, type);
+	if (type == 's')
+		dprintf(STDERR_FILENO, message, va_arg(args, char *));
+	else if (type == 'd')
+		dprintf(STDERR_FILENO, message, va_arg(args, int));
+	else if (type == 'N')
+		dprintf(STDERR_FILENO, message, "");
+	else
+		dprintf(STDERR_FILENO, "Error: Does not match any type\n");
+	va_end(args);
+	exit(exit_code);
+}
+
+/**
+ * main - copies the content of a file to another file
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return:  0 (Always)
+ */
+
+int main(int argc, char *argv[])
+{
+	char *src, *dest, buffer[1024];
+	int fd_s, fd_d;
+
+
+	buffer[0] = '\0';
+	printf("%c\n", buffer[0]);
+
+
+	if (argc != 3)
+		error_handler(97, "Usage: cp file_from file_to\n", 'N');
+
+	src = argv[1];
+	dest = argv[2];
+
+	fd_s = open(src, O_RDONLY);
+	if (fd_s == -1)
+		error_handler(98, "Error: Can't read from file %s\n", 's', src);
+
+	if (close(fd_s) == -1)
+		error_handler(100, "Error: Can't close fd %d\n", 'd', fd_s);
+
+	fd_d = open(dest, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	if (fd_d == -1)
+		error_handler(99, "Error: Can't write to %s\n", 's', dest);
+
+	if (close(fd_d) == -1)
+		error_handler(100, "Error: Can't close fd %d\n", 'd', fd_d);
+
+	return (0);
+}
--- 0x14-file_io/README.md
+++ 0x14-file_io/README.md
@@ -2,3 +2,4 @@
 * 0-read_textfile.c: function that reads a text file and prints it to the POSIX standard output
 * 1-create_file.c: function that creates a file
 * 2-append_text_to_file.c: function that appends a text at the end of a file
+* 3-cp.c: program that copies the content of a file to another file
