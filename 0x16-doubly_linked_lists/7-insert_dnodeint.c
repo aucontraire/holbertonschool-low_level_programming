b5e77357d4d93172e570a11e9f8ea7cd3d7ac0aa
Omar <ocontreras.sf@gmail.com>
2018-04-18 00:47:51 +0000

    Account for NULL head

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -50,6 +50,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	new->n = n;
 
+	if (*h == NULL)
+	{
+		new->prev = NULL;
+		new->next = NULL;
+		*h = new;
+		return (new);
+	}
+
 	temp = *h;
 	while (temp)
 	{
