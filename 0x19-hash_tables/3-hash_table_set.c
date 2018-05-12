6e9890792bf7d6bcbd860afdcc9d7120ffb481a1
Omar <ocontreras.sf@gmail.com>
2018-05-12 18:46:06 +0000

    Add collision insurance

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -12,17 +12,26 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *node;
+	hash_node_t *node, *head;
 
 	index = key_index((const unsigned char *)key, ht->size);
 
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 		return (0);
-
 	node->key = strdup(key);
 	node->value = strdup(value);
 	node->next = NULL;
-	ht->array[index] = node;
+
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = node;
+	}
+	else
+	{
+		head = ht->array[index];
+		node->next = head;
+		ht->array[index] = node;
+	}
 	return (1);
 }
