7a1928f3c98198d16d7c40782808e267ad422281
Omar <ocontreras.sf@gmail.com>
2018-05-12 04:41:36 +0000

    Add function that adds an element to the hash table

---
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table
+ * @key: key
+ * @value: value paired with key
+ *
+ * Return: 1 if success, 0 otherwise
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
+
+	node->key = strdup(key);
+	node->value = strdup(value);
+	node->next = NULL;
+	ht->array[index] = node;
+	return (1);
+}
