f524720115710387f5b86ac7265da3d228a603ab
Omar <ocontreras.sf@gmail.com>
2018-05-12 00:32:08 +0000

    Add semicolons

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -15,12 +15,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (size < 1)
 		return (NULL);
 
-	ht = malloc(sizeof(hash_table_t))
+	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
-	ht->array = malloc(sizeof(hash_node_t *) * size)
+	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
 		return (NULL);
+
 	for (index = 0; index < size; index++)
 		ht->array[index] = NULL;
 	return (ht);
