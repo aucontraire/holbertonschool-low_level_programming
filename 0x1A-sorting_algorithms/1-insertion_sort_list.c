23bd9a4c52cb49884b65fa1b84e936e4d8fc2425
Omar <ocontreras.sf@gmail.com>
2018-05-29 18:05:12 +0000

    Add function that uses insertion sort to sort doubly linked list

---
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list with insertion sort
+ * @list: doubly linked list
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *key;
+
+	if (*list == NULL || (*list)->next == NULL)
+		return;
+
+	key = (*list)->next;
+	while (key != NULL)
+	{
+		while (key->prev != NULL && key->n < key->prev->n)
+		{
+			key->prev->next = key->next;
+			if (key->next != NULL)
+				key->next->prev = key->prev;
+			key->next = key->prev;
+			key->prev = key->prev->prev;
+			key->next->prev = key;
+			if (key->prev == NULL)
+				*list = key;
+			else
+				key->prev->next = key;
+			print_list(*list);
+		}
+		key= key->next;
+	}
+}
--- 0x1A-sorting_algorithms/README.md
+++ 0x1A-sorting_algorithms/README.md
@@ -1,3 +1,5 @@
 # C - Sorting algorithms & Big O
 * 0-bubble_sort.c, 0-O: function that sorts an array of integers in ascending order using the Bubble sort algorithm
+* 1-insertion_sort_list.c, 1-O: function that sorts a doubly linked list of integers in ascending order using the Insertion sort algorithm
 * 2-selection_sort.c, 2-O: function that sorts an array of integers in ascending order using the Selection sort algorithm
+* 3-quick_sort.c, 3-O:  function that sorts an array of integers in ascending order using the Quick sort algorithm
