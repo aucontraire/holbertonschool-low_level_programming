7ada2ae7fa73b99a3b9479006f051802a89009a4
Omar <ocontreras.sf@gmail.com>
2018-05-30 22:48:28 +0000

    Add header file

--- 0x1A-sorting_algorithms/sort.h
+++ 0x1A-sorting_algorithms/sort.h
@@ -26,5 +26,6 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
 
 #endif
