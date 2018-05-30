8b81edfd98de20ae2f3d93e5d4bca0acb65d34fb
Omar <ocontreras.sf@gmail.com>
2018-05-30 22:56:51 +0000

    Add frees and add find_max function

--- 0x1A-sorting_algorithms/102-counting_sort.c
+++ 0x1A-sorting_algorithms/102-counting_sort.c
@@ -1,6 +1,27 @@
 #include "sort.h"
 
 /**
+ * find_max - find max integer in array
+ * @array: array
+ * @size: size of array
+ *
+ * Return: max integer
+ */
+
+int find_max(int *array, size_t size)
+{
+	int max = 0;
+	size_t i = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		if (max < array[i])
+			max = array[i];
+	}
+	return max;
+}
+
+/**
  * counting_sort - sort array using counting sort algorithm
  * @array: array
  * @size: size of array
@@ -9,31 +30,30 @@
 void counting_sort(int *array, size_t size)
 {
 	size_t i = 0;
-	int j = 0;
-	int max = 0;
-	int *counts;
-	int *sums;
-	int *sorted;
-	int tally = 0;
+	int j = 0, max = 0, tally = 0;
+	int *counts, *sums, *sorted;
 
 	if (array == NULL)
 		return;
 	if (size < 2)
 		return;
-	for (i = 0; i < size; i++)
-	{
-		if (max < array[i])
-			max = array[i];
-	}
+	max = find_max(array, size);
 	counts = malloc(sizeof(int) * (max + 1));
 	if (counts == NULL)
 		return;
 	sums = malloc(sizeof(int) * (max + 1));
 	if (sums == NULL)
+	{
+		free(counts);
 		return;
+	}
 	sorted = malloc(sizeof(int) * size);
 	if (sorted == NULL)
+	{
+		free(counts);
+		free(sums);
 		return;
+	}
 	for (j = 0; j <= max; j++)
 		counts[j] = 0;
 	for (i = 0; i < size; i++)
