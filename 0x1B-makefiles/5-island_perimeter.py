1ce93ac1c755a31edb8aa1fb371248635c86734a
Omar <ocontreras.sf@gmail.com>
2018-06-16 03:19:58 +0000

    Account for -1 index that references the last item in a line

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -2,6 +2,39 @@
 """island_perimeter function"""
 
 
+def try_the_edges(r, c, grid):
+    """checks perimeter of individual cells for water (0)
+    Args:
+        r (int): row coordinate
+        c (int): column coordinate
+        grid (list): list of lists
+    Returns:
+        count of zeroes or sides cell is surrounded by water
+    """
+    count = 0
+    print('({},{})'.format(r, c))
+
+    if r == 0:
+        count += 1
+    if r == len(grid) - 1:
+        count += 1
+    if c == 0:
+        count += 1
+    if c == len(grid[0]) - 1:
+        count += 1
+
+    if r > 0 and r < len(grid) - 1 and c > 0 and c < len(grid[0]) - 1:
+        if grid[r - 1][c] == 0:
+            count += 1
+        if grid[r][c + 1] == 0:
+            count += 1
+        if grid[r + 1][c] == 0:
+            count += 1
+        if grid[r][c - 1] == 0:
+            count += 1
+    return count
+
+
 def island_perimeter(grid):
     """perimeter of island (1) surrounded by water (0)
     Args:
@@ -10,23 +43,36 @@ def island_perimeter(grid):
         perimeter number of cells the island is surrounded by water
     """
     perimeter = 0
+
+    if not grid:
+        return perimeter
+    if type(grid) is not list:
+        return perimeter
     rows = len(grid)
-    if rows < 3:
+    if rows < 1:
         return perimeter
     columns = len(grid[0])
-    if columns < 3:
-        return perimeter
 
-    for r in range(1, rows - 1):
-        for c in range(1, columns - 1):
+    for r in range(rows):
+        for c in range(columns):
             if grid[r][c] == 1:
-                if grid[r - 1][c] == 0:
-                    perimeter += 1
-                if grid[r][c + 1] == 0:
-                    perimeter += 1
-                if grid[r + 1][c] == 0:
-                    perimeter += 1
-                if grid[r][c - 1] == 0:
-                    perimeter += 1
+                perimeter += try_the_edges(r, c, grid)
 
     return perimeter
+
+if __name__ == "__main__":
+
+    grid = [
+        [1, 1],
+        [1, 1]
+    ]
+
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+
+    print(island_perimeter(grid))
