4bdfacbc2fedb54c6f5a9952076a9ff77e48f4e6
Omar <ocontreras.sf@gmail.com>
2018-08-03 06:34:08 +0000

    Use recursion

--- 0x1D-search_algorithms/104-advanced_binary.c
+++ 0x1D-search_algorithms/104-advanced_binary.c
@@ -19,33 +19,35 @@ void print_subarray(int *array, size_t first, size_t last)
 	printf("%d\n", array[i]);
 }
 
+
 /**
- * advanced_binary - search for value in sorted int array w Binary search alg
+ * bin_search_mod - search for val in sorted int array using Binary search alg
  * @array: pointer to int array
- * @size: size of array
  * @value: value to find
+ * @first: first index of subarray
+ * @last: last index of subarray
  * Return: index of first match or -1 if not found
  */
 
-int advanced_binary(int *array, size_t size, int value)
+int bin_search_mod(int *array, int value, int first, int last)
 {
-	size_t first = 0;
-	size_t last = 0;
 	size_t mid = 0;
-
-	if (array == NULL || size == 0)
-		return (-1);
-
-	last = size - 1;
 
 	while (first <= last)
 	{
 		print_subarray(array, first, last);
 		mid = (first + last) / 2;
 		if (array[mid] > value)
+		{
 			last = mid - 1;
+			bin_search_mod(array, value, first, last);
+
+		}
 		else if (array[mid] < value)
+		{
 			first = mid + 1;
+			bin_search_mod(array, value, first, last);
+		}
 		else
 		{
 			if (array[mid - 1] != value)
@@ -58,4 +60,26 @@ int advanced_binary(int *array, size_t size, int value)
 		}
 	}
 	return (-1);
+}
+
+
+/**
+ * advanced_binary - search for value in sorted int array w Binary search alg
+ * @array: pointer to int array
+ * @size: size of array
+ * @value: value to find
+ * Return: index of first match or -1 if not found
+ */
+
+int advanced_binary(int *array, size_t size, int value)
+{
+	size_t first = 0;
+	size_t last = 0;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	last = size - 1;
+	return (bin_search_mod(array, value, first, last));
+
 }
