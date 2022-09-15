341548f33b8afee086913ee71150432a8bf60004
Precious <45766007+PreciousDipe@users.noreply.github.com>
2022-09-15 23:44:15 +0100

    Update 9-fizz_buzz.c

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,12 +1,10 @@
 #include "holberton.h"
 #include <stdio.h>
-
 /**
  * main - take the fizz-buzz challenge
  *
  * Return: Always 0
  */
-
 int main(void)
 {
 	int n;
@@ -26,6 +24,5 @@ int main(void)
 			printf("%d ", n);
 	}
 	printf("\n");
-
 	return (0);
 }
